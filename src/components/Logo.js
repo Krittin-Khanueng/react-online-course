a68736d37b13e389bc784e73f1b9f7682ce0cd95
Krittin <6110210013@psu.ac.th>
2021-03-28 23:05:56 +0700

    add style components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 ﻿import React from 'react'
-
+import { logo } from '../styles/style'
 const Logo = () => {
     const logoImage = {
         url: './logo192.png'
     }
     return (
         <div>
-           <img src={logoImage.url} width="`100" alt="logo" />
+           <img style={logo}  src={logoImage.url} width="`100" alt="logo" />
         </div>
     )
 }
