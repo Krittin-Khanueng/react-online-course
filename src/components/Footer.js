2abbd632c89d87c6852d02d79fca9837b22bbe3b
Krittin Khanung <6110210013@psu.ac.th>
2021-04-12 15:16:47 +0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,13 @@
-﻿import React from 'react'
-import PropTypes from 'prop-types'
+﻿import React from "react";
 
-const Footer = ({title, website, postcode, isOpen}) => {
-    //const {title, website, postcode} = props;
-    return (
-        <div>
-            <h1 style={styles.title}>{title} &copy; {new Date().getFullYear()}</h1>
-            <p style={{color: 'green', fontSize:16}}>{website} {postcode} isOpen:{isOpen.toString()}</p>
-            <p style={styles.title}>codingthailand</p>
-        </div>
-    )
-}
-
-const styles ={
-    title:{
-        color: 'red'
-    }
-}
-
-Footer.propTypes= {
-    title: PropTypes.string,
-    website: PropTypes.string,
-    postcode: PropTypes.number,
-    isOpen: PropTypes.bool
+const Footer = ({ title, website, postcode, isOpen }) => {
+  return (
+    <>
+      <footer className="container">
+        <p>© Company 2017-{new Date().getFullYear}</p>
+      </footer>
+    </>
+  );
 };
 
-export default Footer
+export default Footer;
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,18 @@
 ﻿import React from 'react'
 import { logo } from '../styles/style'
+import useHover from '../hooks/UseHover'
+
 const Logo = () => {
+    const [hover,attrs] = useHover()
     const logoImage = {
         url: './logo192.png'
     }
     return (
         <div>
-           <img style={logo}  src={logoImage.url} width="`100" alt="logo" />
+            {
+                hover ? <p>Hello Logo</p> : null 
+            }
+           <img {...attrs} style={logo}  src={logoImage.url} width="`100" alt="logo" />
         </div>
     )
 }
